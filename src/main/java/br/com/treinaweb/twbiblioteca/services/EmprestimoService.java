c0256714cc650a9047db24c8d182932b9ae626ea
jvictornascimento <victojoao789@gmail.com>
2024-06-25 20:19:46 -0300

    second TDD part

--- src/main/java/br/com/treinaweb/twbiblioteca/services/EmprestimoService.java
+++ src/main/java/br/com/treinaweb/twbiblioteca/services/EmprestimoService.java
@@ -4,10 +4,18 @@ import br.com.treinaweb.twbiblioteca.models.Cliente;
 import br.com.treinaweb.twbiblioteca.models.Emprestimo;
 import br.com.treinaweb.twbiblioteca.models.Obra;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public class EmprestimoService {
     public Emprestimo novo(Cliente cliente, List<Obra> obras){
-        return null;
+        var emprestimo = new Emprestimo();
+
+        emprestimo.setCliente(cliente);
+        emprestimo.setLivros(obras);
+        emprestimo.setDataEmprestimo(LocalDate.now());
+        emprestimo.setDataDevolucao(LocalDate.now().plusDays(3));
+
+        return emprestimo;
     }
 }
