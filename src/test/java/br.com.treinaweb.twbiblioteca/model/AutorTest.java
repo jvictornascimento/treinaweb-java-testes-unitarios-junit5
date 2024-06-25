150b5b3139dcdc88d01800850f95f9f5aefc3834
jvictornascimento <victojoao789@gmail.com>
2024-06-25 18:28:46 -0300

    testing the two parts

--- src/test/java/br.com.treinaweb.twbiblioteca/model/AutorTest.java
+++ src/test/java/br.com.treinaweb.twbiblioteca/model/AutorTest.java
@@ -4,14 +4,29 @@ import br.com.treinaweb.twbiblioteca.models.Autor;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 public class AutorTest {
     @Test
-    public void testaMedotoEstaVivo(){
+    void quandoMetodoEstaVivoForChamadoComDataFalecimentoNulaDeveRetornarTrue(){
         var autor =new Autor();
 
         var estaVivo = autor.estaVivo();
-        Assertions.assertEquals(true,estaVivo);
+        assertEquals(true,estaVivo);
+    }
+    @Test
+    void quandoMetodoForChamadoComDataFalecimetnoPreenchidaDeveRetornarFalse(){
+        //cenario
+        var autor = new Autor();
+        autor.setDataFalecimento(LocalDate.now());
 
+        //execução
+        var estaVivo= autor.estaVivo();
 
+        //verificação
+        assertEquals(false,estaVivo);
     }
 }
